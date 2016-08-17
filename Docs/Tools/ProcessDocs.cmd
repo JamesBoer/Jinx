8ec55de6cfddbf517c101f3d93aad9683f5e8869
JamesBoer <james.boer@gmail.com>
2016-08-17 10:09:08 -0700

    Tweaked documents for readability

--- Docs/Tools/ProcessDocs.cmd
+++ Docs/Tools/ProcessDocs.cmd
@@ -1,6 +1,5 @@
 @echo off
 
 rem Add Courier as a backup font to Consolas, which may not be available on all systems
-powershell -Command "(gc ../Overview.htm) -replace 'Consolas;', 'Consolas, Hack, Inconsolata, Monaco, monospace;' | Out-File ../Overview.htm"
 powershell -Command "(gc ../LibCore.htm) -replace 'Consolas;', 'Consolas, Hack, Inconsolata, Monaco, monospace;' | Out-File ../LibCore.htm"
 powershell -Command "(gc ../Tutorial.htm) -replace 'Consolas;', 'Consolas, Hack, Inconsolata, Monaco, monospace;' | Out-File ../Tutorial.htm"
