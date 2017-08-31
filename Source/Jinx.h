fd654745ba7fdd246ea868caa5d2ae4dc3cf0bda
JamesBoer <james.boer@gmail.com>
2017-08-31 14:02:23 -0700

    Update documentation comments

--- Source/Jinx.h
+++ Source/Jinx.h
@@ -359,8 +359,8 @@ namespace Jinx
 
 	/// Initializes global Jinx parameters
 	/**
-	\param params A GlobalParams struct containing a number of parameters that affect any
-	created runtime objects.
+	A struct containing a number of global parameters affecting memory, logging, and
+	general runtime behavior.
 	\sa Initialize()
 	*/
 	struct GlobalParams
@@ -392,7 +392,7 @@ namespace Jinx
 		size_t allocBlockSize;
 		/// Number of spare allocation blocks to retain
 		size_t allocSpareBlocks;
-		/// Maximum number of instructions per tick
+		/// Maximum number of instructions per script per Execute() function
 		uint32_t maxInstructions;
 		/// Maximum total script instrunctions
 		bool errorOnMaxInstrunctions;
