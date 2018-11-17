8b7d45bfa7d9df6b1952d673dc8f0e045f5b98fb
James Boer <james.boer@gmail.com>
2018-11-17 11:04:47 -0800

    Change script names
    Adjust to reflect generator targets instead of platforms.

--- Bin/BuildMSVC.cmd
+++ Bin/BuildMSVC.cmd
@@ -5,37 +5,37 @@ cd Build/
 rem build main Jinx library
 mkdir Jinx
 cd Jinx/
-cmake ../../ -A x64
+cmake ../../ -G "Visual Studio 15 2017" -A x64
 cd ..
 
 rem build features test app
 mkdir Features
 cd Features/
-cmake ../../Tests/Features/ -A x64
+cmake ../../Tests/Features/ -G "Visual Studio 15 2017" -A x64
 cd ..
 
 rem build fuzz test app
 mkdir FuzzTests
 cd FuzzTests/
-cmake ../../Tests/FuzzTests/ -A x64
+cmake ../../Tests/FuzzTests/ -G "Visual Studio 15 2017" -A x64
 cd ..
 
 rem build header test app
 mkdir HeaderTest
 cd HeaderTest/
-cmake ../../Tests/HeaderTest/ -A x64
+cmake ../../Tests/HeaderTest/ -G "Visual Studio 15 2017" -A x64
 cd ..
 
 rem build perf test app
 mkdir PerfTest
 cd PerfTest/
-cmake ../../Tests/PerfTest/ -A x64
+cmake ../../Tests/PerfTest/ -G "Visual Studio 15 2017" -A x64
 cd ..
 
 rem build unit tests app
 mkdir UnitTests
 cd UnitTests/
-cmake ../../Tests/UnitTests/ -A x64
+cmake ../../Tests/UnitTests/ -G "Visual Studio 15 2017" -A x64
 cd ..
 
 cd ..
