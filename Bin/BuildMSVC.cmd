f02835bec1e80fcb95314e6f30a178e7408a8185
James Boer <james.boer@gmail.com>
2020-08-12 02:03:23 -0700

    Slightly modified error handling in batch

--- Bin/BuildMSVC.cmd
+++ Bin/BuildMSVC.cmd
@@ -6,20 +6,21 @@ MSBuild Jinx.sln /p:Configuration=Debug && (
   echo Debug build succeeded
 ) || (
   echo Debug build failed
-  cd ..
-  cd Bin
-  EXIT /B 1
+  goto ERROR
 )
 
 MSBuild Jinx.sln /p:Configuration=Release && (
   echo Release build succeeded
 ) || (
   echo Release build failed
-  cd ..
-  cd Bin
-  EXIT /B 1
+  goto ERROR
 )
 
 cd ..
 cd Bin
 EXIT /B 0
+
+:ERROR
+cd ..
+cd Bin
+EXIT /B 1
