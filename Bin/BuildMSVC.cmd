f39d90813d9559f7f25d6429da2ca3f226dd9d84
James Boer <james.boer@gmail.com>
2020-08-12 01:34:44 -0700

    Added error handling to batch file

--- Bin/BuildMSVC.cmd
+++ Bin/BuildMSVC.cmd
@@ -1,7 +1,22 @@
 call GenerateMSVC.cmd
 cd ..
 cd Build
-MSBuild Jinx.sln /p:Configuration=Debug
-MSBuild Jinx.sln /p:Configuration=Release
+
+MSBuild Jinx.sln /p:Configuration=Debug && (
+  echo Debug build succeeded
+) || (
+  echo Debug build failed
+  goto FINAL
+)
+
+MSBuild Jinx.sln /p:Configuration=Release && (
+  echo Release build succeeded
+) || (
+  echo Release build failed
+  goto FINAL
+)
+
+:FINAL
 cd ..
 cd Bin
+EXIT /B %ERRORLEVEL%
